4cc5fa63305f025dffc6dca8693dd8e497fc8eb4
ethan <ethan@ubuntu>
2017-02-05 18:42:25 -0800

    add install instruction

---
+++ src/CMakeFiles/CLOG.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLOG.dir/log.cpp.o"
+  "CMakeFiles/CLOG.dir/LogFile.cpp.o"
+  "libCLOG.pdb"
+  "libCLOG.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLOG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
